6ac76fdce7ab5b2f455d005b7c9c462fba6f6299
Poncho0794 <benjamin.banda.reyna@hotmail.com>
2016-03-22 16:23:05 -0600

    Interfaz de settings y cominezo de preferences

--- app/src/main/java/com/rhynyx/cashtool/fragments/Settings.java
+++ app/src/main/java/com/rhynyx/cashtool/fragments/Settings.java
@@ -1,11 +1,15 @@
 package com.rhynyx.cashtool.fragments;
 
+import android.app.SharedElementCallback;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 import com.rhynyx.cashtool.R;
 
@@ -13,10 +17,21 @@ import com.rhynyx.cashtool.R;
  * Created by juan on 5/03/16.
  */
 public class Settings extends Fragment {
+    Switch notiSwitch;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.content_settings,container,false);
+        View v = inflater.inflate(R.layout.content_settings, container, false);
+        notiSwitch = (Switch)v.findViewById(R.id.switchSet);
+        Context context = getActivity();
+        SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.preference), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        boolean notStatus = notiSwitch.isChecked();
+        if (notStatus){
+            editor.putString("Status","On");
+        }else {
+            editor.putString("Status", "Off");
+        }
         return v;
     }
 }
